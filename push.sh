4b0e5a9cfc0ff33141e7fe448582cddddf853cee
Liszy Szilveszter <szilveszter@liszy.hu>
2022-06-21 15:05:35 +0200

    changes

--- push.sh
+++ push.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env sh
 
-pwd
 git config --global --add safe.directory /github/workspace
-git remote add mirror "$TOKEN_URL"
+git remote add mirror $TOKEN_URL
 git push --tags --force --prune mirror "refs/remotes/origin/*:refs/heads/*"
